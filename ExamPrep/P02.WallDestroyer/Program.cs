8d1ec547e9749e93ab93f20cba0d43e7440c3cb0
Zdravko Ivanov <zdravko.d.ivanov@gmail.com>
2022-09-19 17:06:39 +0300

    Changes

---
+++ ExamPrep/P02.WallDestroyer/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace P02.WallDestroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vankoPosRow = 0;
+            int vankoPosCol = 0;
+            int countHoles = 1;
+            int countRods = 0;
+            bool isElectrocuted = true;
+
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            FillMatrix();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End" && isElectrocuted)
+            {
+                if (command == "up")
+                {
+                    MoveTo(vankoPosRow - 1, vankoPosCol);
+                }
+                else if (command == "down")
+                {
+
+                    MoveTo(vankoPosRow + 1, vankoPosCol);
+                }
+                else if (command == "left")
+                {
+
+                    MoveTo(vankoPosRow, vankoPosCol - 1);
+                }
+                else if (command == "right")
+                {
+
+                    MoveTo(vankoPosRow, vankoPosCol + 1);
+                }
+            }
+
+            if (!isElectrocuted)
+            {
+                Console.WriteLine($"Vanko got electrocuted, but he managed to make {countHoles} hole(s).");
+                PrintMatrix();
+            }
+            else
+            {
+                Console.WriteLine($"Vanko managed to make {countHoles} hole(s) and he hit only {countRods} rod(s).");
+                PrintMatrix();
+            }
+
+
+
+
+            void FillMatrix()
+            {
+                for (int rows = 0; rows < n; rows++)
+                {
+                    var row = Console.ReadLine().ToArray();
+                    for (int cols = 0; cols < n; cols++)
+                    {
+                        matrix[rows, cols] = row[cols];
+                        if (matrix[rows, cols] == 'V')
+                        {
+                            vankoPosRow = rows;
+                            vankoPosCol = cols;
+                        }
+                    }
+
+                }
+            }
+
+            void MoveTo(int newRow, int newCol)
+            {
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n)
+                {
+                    if (matrix[newRow, newCol] == '*')
+                    {
+                        Console.WriteLine($"The wall is already destroyed at position [{newRow}, {newCol}]!");
+                        matrix[newRow, newCol] = 'V';
+                        matrix[vankoPosRow, vankoPosCol] = '*';
+                        vankoPosRow = newRow;
+                        vankoPosCol = newCol;
+                    }
+
+                    else if (matrix[newRow, newCol] == 'R')
+                    {
+                        Console.WriteLine("Vanko hit a rod!");
+                        countRods++;
+                    }
+                    else if (matrix[newRow, newCol] == 'C')
+                    {
+                        matrix[newRow, newCol] = 'E';
+                        matrix[vankoPosRow, vankoPosCol] = '*';
+                        countHoles++;
+                        isElectrocuted = false;
+                    }
+                    else
+                    {
+                        countHoles++;
+                        matrix[newRow, newCol] = 'V';
+                        matrix[vankoPosRow, vankoPosCol] = '*';
+                        vankoPosRow = newRow;
+                        vankoPosCol = newCol;
+                    }
+                }
+            }
+
+
+            void PrintMatrix()
+            {
+                for (int rows = 0; rows < n; rows++)
+                {
+
+                    for (int cols = 0; cols < n; cols++)
+                    {
+                        Console.Write(matrix[rows, cols]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+
+    }
+}
---
+++ ExamPrep/Renovators/Catalog.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+        public int Count { get; set; }
+        public List<Renovator> Renovators { get; set; }
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            this.Name = name;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+            this.Renovators = new List<Renovator>();
+        }
+
+        public string AddRenovator(Renovator renovator)
+        {
+
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+            {
+                return "Invalid renovator's inforarmation.";
+            }
+            if (this.Count >= this.NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+            else
+            {
+                this.Count++;
+                this.Renovators.Add(renovator);
+                return $"Successfully added {renovator.Name} to the catalog.";
+            }
+
+        }
+        public bool RemoveRenovator(string name)
+        {
+            int isthereany = this.Renovators.RemoveAll(x => x.Name == name);
+            if (isthereany != 0)
+            {
+                this.Count -= isthereany;
+                return true;
+            }
+            else return false;
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            int temp = this.Renovators.RemoveAll(x => x.Type == type);
+            this.Count -= temp;
+            return temp;
+        }
+        public Renovator HireRenovator(string name)
+        {
+            foreach (var renovator in Renovators)
+            {
+                if (renovator.Name == name)
+                {
+                    renovator.Hired = true;
+                    return renovator;
+                }
+            }
+            return null;
+        }
+        public List<Renovator> PayRenovators(int days)
+        {
+            return this.Renovators.Where(x => x.Days >= days).ToList();
+        }
+
+        public string Report()
+        {
+            return $"Renovators available for Project {this.Project}:" + Environment.NewLine +
+            $"{string.Join(Environment.NewLine, this.Renovators.Where(x => x.Hired == false))}";
+        }
+
+    }
+}
